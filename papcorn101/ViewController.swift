784bce5288c82ac3dd4f94b877b06a5f835c5bfa
ismailtever <ismailtever94@gmail.com>
2023-08-21 16:56:40 +0300

    Initial Commit

---
+++ papcorn101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  papcorn101
+//
+//  Created by Ismail Tever on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
