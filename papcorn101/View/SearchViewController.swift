29c3c5311c092c82eb72b7b306bf861040bbe549
ismailtever <ismailtever94@gmail.com>
2023-08-23 14:31:07 +0300

    feat: tab bar with navigation

---
+++ papcorn101/View/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  papcorn101
+//
+//  Created by Ismail Tever on 22.08.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+    
+
+   
+
+}
